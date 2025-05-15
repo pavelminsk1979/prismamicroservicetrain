de5343133273b1064f66435451c3c0d0e7cf368d
pavvelpotapov <pavelminsk1979@mail.ru>
2025-05-15 16:14:11 +0300

    first commit

--- apps/microcervprisma/src/main.ts
+++ apps/microcervprisma/src/main.ts
@@ -3,6 +3,6 @@ import { MicrocervprismaModule } from './microcervprisma.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MicrocervprismaModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3002);
 }
 bootstrap();
