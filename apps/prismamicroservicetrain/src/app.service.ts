5fb73f852e0061d965cea4794a8941ca178b2db4
pavvelpotapov <pavelminsk1979@mail.ru>
2025-05-16 15:50:15 +0300

    commit 3

--- apps/prismamicroservicetrain/src/app.service.ts
+++ apps/prismamicroservicetrain/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prismaservice';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!3001';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getHello() {
+    const res = await this.prisma.moto.create({
+      data: {
+        moto1: 'some title123',
+        moto2: 'some title111111',
+        moto3: 'some title2222222',
+        ugauga: 'ugauga',
+      },
+    });
+    console.log(res);
+    return res;
   }
 }
